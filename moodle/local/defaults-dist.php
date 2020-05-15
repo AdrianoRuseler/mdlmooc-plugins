abb5e67d2534b55768b42b0a6cec2da17a9a3f97
Adriano Ruseler <ruseler@utfpr.edu.br>
2020-05-15 18:41:22 -0300

    Create defaults-dist.php

---
+++ moodle/local/defaults-dist.php
@@ -0,0 +1,20 @@
+<?php
+$defaults['moodle']['summary'] = 'Moodle Test Site! '; // for core settings
+$defaults['moodle']['noreplyaddress'] = 'moodletestutfpr@gmail.com';
+$defaults['moodle']['custommenuitems'] = 'Tema
+-Classic | MYPUBHOSTURL/?theme=classic 
+-Boost | MYPUBHOSTURL/?theme=boost 
+-Moove | MYPUBHOSTURL/?theme=moove 
+';
+
+$defaults['moodle']['pathtophp'] = '/usr/bin/php';
+$defaults['moodle']['pathtodu'] = '/usr/bin/du';
+$defaults['moodle']['aspellpath'] = '/usr/bin/aspell';
+$defaults['moodle']['pathtogs'] = '/usr/bin/gs';
+$defaults['moodle']['pathtodot'] = '/usr/bin/dot';
+
+$defaults['moodle']['smtphosts'] = 'smtp.mailgun.org:587';
+$defaults['moodle']['smtpsecure'] = 'tls';
+$defaults['moodle']['smtpauthtype'] = 'LOGIN';
+$defaults['moodle']['smtpuser'] = 'postmaster@sandboxe2fd1252a8774a0a801812ad8376cdf6.mailgun.org';
+$defaults['moodle']['smtppass'] = 'adb8d24d5bbc1fdd6fe42d25b38862ba-9dfbeecd-e7fd391d';
