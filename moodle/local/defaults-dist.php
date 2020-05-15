713a24eb117b2657d750b9ac6bc084e11f1a764f
Adriano Ruseler <ruseler@utfpr.edu.br>
2020-05-15 19:17:30 -0300

    Update defaults-dist.php

--- moodle/local/defaults-dist.php
+++ moodle/local/defaults-dist.php
@@ -5,6 +5,7 @@ $defaults['moodle']['custommenuitems'] = 'Tema
 -Classic | MYPUBHOSTURL/?theme=classic 
 -Boost | MYPUBHOSTURL/?theme=boost 
 -Moove | MYPUBHOSTURL/?theme=moove 
+Solicitação de curso | MYPUBHOSTURL/course/request.php
 ';
 
 $defaults['moodle']['pathtophp'] = '/usr/bin/php';
@@ -13,8 +14,3 @@ $defaults['moodle']['aspellpath'] = '/usr/bin/aspell';
 $defaults['moodle']['pathtogs'] = '/usr/bin/gs';
 $defaults['moodle']['pathtodot'] = '/usr/bin/dot';
 
-$defaults['moodle']['smtphosts'] = 'smtp.mailgun.org:587';
-$defaults['moodle']['smtpsecure'] = 'tls';
-$defaults['moodle']['smtpauthtype'] = 'LOGIN';
-$defaults['moodle']['smtpuser'] = 'postmaster@sandboxe2fd1252a8774a0a801812ad8376cdf6.mailgun.org';
-$defaults['moodle']['smtppass'] = 'adb8d24d5bbc1fdd6fe42d25b38862ba-9dfbeecd-e7fd391d';
