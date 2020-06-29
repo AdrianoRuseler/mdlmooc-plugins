fbb0109118c21e5e349b46e2d0744fb9fb7d90ce
Adriano Ruseler <ruseler@utfpr.edu.br>
2020-06-29 16:57:36 -0300

    Update defaults-dist.php

---
+++ scripts/test/defaults-dist.php
@@ -0,0 +1,15 @@
+<?php
+$defaults['moodle']['summary'] = 'Moodle Test Site! '; // for core settings
+$defaults['moodle']['noreplyaddress'] = 'moodletestutfpr@gmail.com';
+$defaults['moodle']['custommenuitems'] = 'Tema
+-Clean | https://mytesturl/?theme=clean
+-Boost | https://mytesturl/?theme=boost';
+
+$defaults['moodle']['timezone'] = 'America/Sao_Paulo';
+$defaults['moodle']['defaultcity'] = 'Curitiba';
+
+$defaults['moodle']['pathtophp'] = '/usr/bin/php';
+$defaults['moodle']['pathtodu'] = '/usr/bin/du';
+$defaults['moodle']['aspellpath'] = '/usr/bin/aspell';
+$defaults['moodle']['pathtogs'] = '/usr/bin/gs';
+$defaults['moodle']['pathtodot'] = '/usr/bin/dot';
