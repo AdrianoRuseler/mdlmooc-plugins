7b4381c6dbf1ea461cbedf9f6a717f3504f34c12
Adriano Ruseler <ruseler@utfpr.edu.br>
2020-07-01 13:00:12 -0300

    Backup improvements and update script

--- scripts/test/moodle-backup.sh
+++ scripts/test/moodle-backup.sh
@@ -7,6 +7,22 @@ MOODLE_DATA="/mnt/mdl/data"  # moodle data folder
 MOODLE_DB="/mnt/mdl/db/data"  # moodle database folder
 MOODLE_HOME="/var/www/moodle/html" # moodle core folder
 
+echo "Run automated backup..."
+sudo -u www-data /usr/bin/php $MOODLE_HOME/admin/cli/automated_backups.php
+
+echo "Kill all user sessions..."
+sudo -u www-data /usr/bin/php $MOODLE_HOME/admin/cli/kill_all_sessions.php
+
+echo "Activating Moodle Maintenance Mode in..."
+sudo -u www-data /usr/bin/php $MOODLE_HOME/admin/cli/maintenance.php --enable
+
+echo "purge Moodle cache..."
+sudo -u www-data /usr/bin/php $MOODLE_HOME/admin/cli/purge_caches.php
+
+echo "fix courses..."
+sudo -u www-data /usr/bin/php $MOODLE_HOME/admin/cli/fix_course_sequence.php -c=* --fix
+
+
 filename=$(date +\%Y-\%m-\%d-\%H.\%M) # Generates filename
 
 # make database backup
@@ -35,3 +51,6 @@ md5sum $DATA_BKP$filename.tar.gz > $DATA_BKP$filename.tar.gz.md5
 md5sum -c $DATA_BKP$filename.tar.gz.md5
 
 ls -lh $DATA_BKP
+
+echo "disable the maintenance mode..."
+sudo -u www-data /usr/bin/php $MOODLE_HOME/admin/cli/maintenance.php --disable
--- scripts/test/userdata-test-ubuntu.sh
+++ scripts/test/userdata-test-ubuntu.sh
@@ -189,7 +189,7 @@ sudo -u www-data /usr/bin/php admin/cli/install_database.php --lang=pt_br --admi
 
 (crontab -l | grep . ; echo -e "*/1 * * * * /usr/bin/php  /var/www/moodle/html/admin/cli/cron.php >/dev/null\n") | crontab -
 
-
 # Install H5P content
 sudo -u www-data /usr/bin/php admin/tool/task/cli/schedule_task.php --execute='\core\task\h5p_get_content_types_task'
+
 
